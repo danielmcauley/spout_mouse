ab99c15a02228fe35a833eba852193eb00bf591d
Daniel McAuley <dmcauley4@gmail.com>
2024-09-02 20:37:24 +0000

    -m fix process_lick_data

--- spout_mouse/data_processing.py
+++ spout_mouse/data_processing.py
@@ -84,30 +84,51 @@ def process_lick_data(data_directory: str) -> pd.DataFrame:
     Raises:
         FileNotFoundError: If no CSV files are found in the directory.
     """
+
     all_files = glob.glob(os.path.join(data_directory, '**', '*.csv'), recursive=True)
     if not all_files:
         raise FileNotFoundError(f"No CSV files found in directory: {data_directory}")
-    
+
     lick_data_list = []
     for file_path in tqdm(all_files, desc="Processing Lick Data"):
-        try:
-            file_name = os.path.basename(file_path)
-            df = pd.read_csv(file_path, usecols=[0], skiprows=1, names=["event_pos_time"]).dropna()
-            df[['event_tag', 'time_ms']] = df['event_pos_time'].str.split(expand=True).astype(int)
-            df = df[df['event_tag'].isin(LICK_CODES + [SPOUT_EXT_CODE, SPOUT_POS_CODE])]
-            df['spout_id'] = df.loc[df['event_tag'] == SPOUT_POS_CODE, 'time_ms']
-            df['spout_id'].ffill(inplace=True)
-            df = df[df['time_ms'] > 0]
-            df['mouse_id'] = file_name.split("_")[3].split(".")[0]
-            df['cohort'] = int(file_path.split(os.sep)[-3].split()[1])
-            df['day'] = int(file_path.split(os.sep)[-4].split()[1])
-            df = df[df['time_ms'] < 5050]
-            lick_data_list.append(df[LICK_DATA_COLS])
-        except Exception as e:
-            print(f"Error processing file {file_path}: {e}")
-    
+
+        lick_data = pd.read_csv(file_path, usecols=[0], skiprows=1, names=["event_pos_time"]).dropna()
+        lick_data[["event_tag", "time_ms"]] = lick_data["event_pos_time"].str.split(expand=True)
+        lick_data.drop(["event_pos_time"], axis=1, inplace=True)
+
+        # Forward fill the 'spout_id' for events where 'event_tag' equals the spout position code
+        lick_data["spout_id"] = lick_data["time_ms"].astype(int) \
+            .where((
+                lick_data["event_tag"].astype(int) == SPOUT_POS_CODE
+            ) & (
+                lick_data["time_ms"].astype(int).isin(VALID_SPOUT_POS)
+            )) \
+            .ffill()    
+
+        lick_data = lick_data.apply(pd.to_numeric)
+        lick_data["mouse_id"] = os.path.basename(file_path).split("_")[3].split(".")[0]
+        lick_data["cohort"] = int(file_path.split("/")[3].split()[1])
+        lick_data["day"] = int(file_path.split("/")[2].split()[1])
+        lick_data = lick_data.loc[lick_data["time_ms"] > 0]
+        lick_data = lick_data[lick_data["event_tag"].isin(LICK_CODES + [SPOUT_EXT_CODE])]
+
+        # Forward fill the 'start_times' for events where 'event_tag' equals the spout extension code
+        start_times = lick_data["time_ms"].where(lick_data["event_tag"] == SPOUT_EXT_CODE).ffill()
+
+        # Adjust 'time_ms' by subtracting the 'start_times' from it
+        lick_data["time_ms"] = lick_data["time_ms"] - start_times
+
+        # Infer the trial number from data ordering
+        lick_data['trial_num'] = (lick_data["event_tag"] == SPOUT_EXT_CODE).astype(int)
+        lick_data['trial_num'] = lick_data.groupby(['mouse_id'])['trial_num'].cumsum()
+
+        lick_data = lick_data.loc[lick_data["time_ms"] < 5050]
+        lick_data_list.append(lick_data[spout_mouse.config.LICK_DATA_COLS  + ["event_tag"]])
+
+
     lick_data_all = pd.concat(lick_data_list, ignore_index=True)
     return lick_data_all
+
 
 
 def compute_lick_rate(lick_data: pd.DataFrame) -> pd.DataFrame:
